29d3b906e2e42afd52fe642ded2de6cadf4e1acb
Audra Tallman <audra.tallman@hotmail.com>
2019-03-19 19:31:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,12 @@
 class Dog 
   def initialize(dogs_name, beed = "Mutt")
     @name = dogs_name
+    @breed = breed
   end 
   def name
     @name
   end 
-  def breed 
-
+  def breed
+    @breed
+  end
 end 
